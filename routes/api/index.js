a52d92b945dea6661d6ebe926d4d2d0609fbf731
Wingz003 <Lsanti618@gmail.com>
2022-01-26 01:48:15 -0600

    test

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const thoughtRoutes = require('./thoughtRoutes');
 const userRoutes = require('./userRoutes');
 
-
 router.use('/thoughts', thoughtRoutes);
 router.use('/users', userRoutes);
 
